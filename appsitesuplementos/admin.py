e4caa90323c0b113ad5e0fd9bfec546b9d50d778
MrGable101 <fredhe2008@hotmail.com>
2022-08-16 21:50:09 -0300

    Corrige criacao dos campos e funcionamento do admin

--- appsitesuplementos/admin.py
+++ appsitesuplementos/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Produtos, Cliente
 
-# Register your models here.
+class ProdutosAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'preco', 'estoque', 'categoria', 'descricao')
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'sobrenome', 'email', 'cpf')
+
+admin.site.register(Produtos, ProdutosAdmin)
+admin.site.register(Cliente, ClienteAdmin)
