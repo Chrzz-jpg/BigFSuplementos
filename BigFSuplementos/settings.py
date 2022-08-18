a60d8970dc72724c91f924fc5a808d253fd0a8a4
gladson.nunes <Mcg4215m*>
2022-08-18 20:54:10 -0300

    Arquivo para o Heroku

--- BigFSuplementos/settings.py
+++ BigFSuplementos/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import dj_database_url
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -77,10 +79,7 @@ WSGI_APPLICATION = 'BigFSuplementos.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default':  dj_database_url.config()
 }
 
 
